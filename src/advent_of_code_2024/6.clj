8bdda2af98c466fe9c1b21a08ebcf2cb32309834
Torstein Røsland <tsteinroesland@gmail.com>
2024-12-06 15:24:52 +0100

    Day 6 Part 1

---
+++ src/advent_of_code_2024/6.clj
@@ -0,0 +1,78 @@
+(ns advent-of-code-2024.6
+  (:require
+   [clojure.string :refer [split-lines]]))
+
+(def input
+  (->>
+   (slurp "resources/input6.txt")
+   (split-lines)))
+
+(defn determine-initial-position-and-direction [lines]
+  (->> lines
+       (keep-indexed
+        (fn [idx-y line]
+          (let [numbers [60 62 94 118]]
+            (->> line
+                 (keep-indexed
+                  #(some (fn [num'] (and (= (int %2) num') [[%1 idx-y] %2]))
+                         numbers))))))
+       (keep seq)
+       (ffirst)
+       (#(cond
+           (= (second %) \v) [(first %) :down]
+           (= (second %) \^) [(first %) :up]
+           (= (second %) % \>) [(first %) :right]
+           (= (second %) % \<) [(first %) :left]))))
+
+(defn increment-position [[x y] direction]
+  (cond
+    (= direction :up) [[x (dec y)] :up]
+    (= direction :down) [[x (inc y)] :down]
+    (= direction :left) [[(dec x) y] :left]
+    (= direction :right) [[(inc x) y] :right]))
+
+(defn change-direction [dir]
+  (cond
+    (= dir :up) :right
+    (= dir :right) :down
+    (= dir :down) :left
+    (= dir :left) :up))
+
+(defn check-for-collision [[x y]]
+  (-> input
+      (get y)
+      (get x)
+      (#(= \# %))))
+
+(defn move-guard [[x y] direction]
+  (loop [pos [x y] direction direction]
+    (let [[new-pos _] (increment-position pos direction)]
+      (if (check-for-collision new-pos)
+        (recur pos (change-direction direction))
+        [new-pos direction]))))
+
+(->
+ (increment-position [4 1] :up)
+ (#(check-for-collision (first %))))
+
+(defn is-valid-position [[x y] input]
+  (-> input
+      (get y)
+      (get x)
+      (#(not (nil? %)))))
+
+(defn determine-positions [input]
+  (loop [[pos dir] (determine-initial-position-and-direction input)
+         positions (seq [[pos dir]])]
+    (let [[new-pos new-dir] (move-guard pos dir)]
+      (if (is-valid-position new-pos input)
+        (recur [new-pos new-dir] (cons [new-pos new-dir] positions))
+        positions))))
+
+(->>
+ (determine-positions input)
+ (map first)
+ (set)
+ (count))
+
+;--- PART 2  ---
