24578f0a5c93c318543ec5e1b526c863c356582d
Torstein Røsland <tsteinroesland@gmail.com>
2024-12-06 11:59:21 +0100

    Day 5, fucking finally, yikes xD

--- src/advent_of_code_2024/5.clj
+++ src/advent_of_code_2024/5.clj
@@ -20,7 +20,7 @@
    (split-lines)
    (map #(map parse-long (split % #",")))))
 
-(defn get-rhs-of-rules [number rules]
+(defn get-lhs-of-rules [number rules]
   (->> rules
        (filter #(= number (second %)))
        (map first)))
@@ -28,7 +28,7 @@
 (defn validate-update [rules update']
   (->> update'
        (map-indexed (fn [idx x]
-                      (let [rhs-rules (get-rhs-of-rules x rules)
+                      (let [rhs-rules (get-lhs-of-rules x rules)
                             later-numbers (drop (inc idx) update')]
                         (not (some #(contains? (set later-numbers) %) rhs-rules)))))
        (#(not (some false? %)))))
@@ -43,30 +43,36 @@
      (reduce +))
 
 ;---- PART 2 ----
-(defn find-collison [update' rule-index rule]
-  (->
-   (keep-indexed (fn [update-idx x]
-                   (if (= x rule)
-                     [rule-index update-idx]
-                     nil))
-                 update')
-   (first)))
+(defn index-of' [e coll] (first (keep-indexed #(when (= e %2) %1) coll)))
 
-(defn fix-update [update' indices index]
-  (let [stuff-to-move (map (partial get (vec update')) indices)
-        prior-numbers (take index update')
-        later-numbers (drop (inc index) update')]
-    (concat prior-numbers stuff-to-move [(get (vec update') index)] (drop (inc (count stuff-to-move)) later-numbers))))
+(defn index-of-first-rule-break [update' rules]
+  (some #(index-of' % update') rules))
 
+(defn fix-with-index [index seq']
+  (let [[f s] (split-at index seq')
+        [problem' rest'] (split-at 1 s)]
+    (concat (drop 1 f)  (concat problem' (take 1 f)) rest')))
 
+(defn fix-update [update' rules]
+  (loop [remaining-update' update' index 0]
+    (if (empty? remaining-update')
+      nil
+      (let [rules-for-index (get-lhs-of-rules (first remaining-update') rules)
+            problem (index-of-first-rule-break remaining-update' rules-for-index)
+            prior-numbers (take index update')]
+        (if (nil? problem)
+          (recur (rest remaining-update') (inc index))
+          (concat prior-numbers (fix-with-index problem remaining-update')))))))
 
+(defn fix-and-find-middle-num [rules update']
+  (loop [update' update' rules rules]
+    (let [fixed-update (fix-update update' rules)]
+      (if (nil? fixed-update)
+        (find-middle-num update')
+        (recur fixed-update rules)))))
 
-
-
-
-
-
-
-
-
-
+(->> updates
+     (map #(or (validate-update rules %) %))
+     (filter #(not (true? %))))
+     ; (map (partial fix-and-find-middle-num rules)) 
+     ; (reduce +)) 
