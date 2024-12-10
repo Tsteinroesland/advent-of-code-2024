1b5039634df32139fd73b3142a22ca471544e099
Torstein Røsland <tsteinroesland@gmail.com>
2024-12-10 10:57:06 +0100

    Day 8

--- src/advent_of_code_2024/8.clj
+++ src/advent_of_code_2024/8.clj
@@ -18,7 +18,7 @@
                        y)))
        (apply concat)))
 
-(def input-height (count input))
+(def dimension (dec (count input)))
 
 (def unique-antenna-groups
   (->>
@@ -31,7 +31,7 @@
     (let [anti-nodes [[(- x1 (- x2 x1)) (- y1 (- y2 y1))]
                       [(+ x2 (- x2 x1)) (+ y2 (- y2 y1))]]
           non-negative-nodes (filter #(not (some neg? %)) anti-nodes)
-          valid-nodes (filter #(not (some (fn [x] (> x (dec input-height))) %)) non-negative-nodes)]
+          valid-nodes (filter #(not (some (fn [x] (> x dimension)) %)) non-negative-nodes)]
 
       (if (> (count valid-nodes) 0)
         valid-nodes
@@ -40,13 +40,10 @@
 (defn antinode-pairs [antenna-group]
   (let [body (second antenna-group)]
     (->>
-     (map (fn [[a1 _]]
-            (keep (fn [[a2 _]] (create-antinodes a1 a2))
-                  body))
-          body)
-     (apply concat)
-     (apply concat))))
-
+     (mapcat (fn [[a1 _]]
+               (mapcat (fn [[a2 _]] (create-antinodes a1 a2))
+                       body))
+             body))))
 (->>
  (map antinode-pairs unique-antenna-groups)
  (apply concat)
@@ -55,14 +52,31 @@
 
 ;-------- PART 2 ---------
 
-(defn create-antinodes-2 [[x1 y1] [x2 y2]]
+(defn recursively-create-antinodes [node delta max']
+  (loop [[x y] node [deltax deltay] delta nodes []]
+    (let [[new-x new-y] [(+ x deltax) (+ y deltay)]]
+      (if (or (neg? new-x) (neg? new-y) (> new-x max') (> new-y max'))
+        nodes
+        (recur [new-x new-y] delta (conj nodes [new-x new-y]))))))
+
+(defn create-antinodes-2 [[x1 y1] [x2 y2] max']
   (if (= [x1 y1] [x2 y2])
     nil
-    (let [anti-nodes [[(- x1 (- x2 x1)) (- y1 (- y2 y1))]
-                      [(+ x2 (- x2 x1)) (+ y2 (- y2 y1))]]
-          non-negative-nodes (filter #(not (some neg? %)) anti-nodes)
-          valid-nodes (filter #(not (some (fn [x] (> x (dec input-height))) %)) non-negative-nodes)]
+    (let [deltax (- x2 x1)
+          deltay (- y2 y1)
+          anti-nodes [(recursively-create-antinodes [x1 y1] (mapv #(* -1 %) [deltax deltay]) max') (recursively-create-antinodes [x1 y1] [deltax deltay] max')]]
+      anti-nodes)))
 
-      (if (> (count valid-nodes) 0)
-        valid-nodes
-        nil))))
+(defn antinode-pairs-2 [antenna-group]
+  (let [body (second antenna-group)]
+    (->>
+     (mapcat (fn [[a1 _]]
+               (mapcat (fn [[a2 _]] (create-antinodes-2 a1 a2 dimension))
+                       body))
+             body))))
+
+(->>
+ (mapcat antinode-pairs-2 unique-antenna-groups)
+ (apply concat)
+ (set)
+ (count))
